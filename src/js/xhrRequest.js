945b9d04a6f19190052a75a6ef790bc59285e9cb
Sidorov Maksim <maxx11167@gmail.com>
2023-05-04 17:07:30 +0300

    feat: change request links

--- src/js/xhrRequest.js
+++ src/js/xhrRequest.js
@@ -5,7 +5,7 @@ export default class XhrRequest {
   sendDataItem(body) {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'http://localhost:7060');
+      xhr.open('POST', 'https://http-backend.onrender.com');
 
       xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
@@ -20,7 +20,7 @@ export default class XhrRequest {
   async getDataForItems(method) {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', `http://localhost:7060/?${method}`);
+      xhr.open('GET', `https://http-backend.onrender.com/?${method}`);
 
       xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
@@ -35,7 +35,7 @@ export default class XhrRequest {
   patchDataForItem(id, body = { nothing: 'nothing' }) {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('PATCH', `http://localhost:7060/?${id}`);
+      xhr.open('PATCH', `https://http-backend.onrender.com/?${id}`);
 
       xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
@@ -54,7 +54,7 @@ export default class XhrRequest {
       if (xhr.readyState !== 4) return false;
     };
 
-    xhr.open('DELETE', `http://localhost:7060/?${id}`);
+    xhr.open('DELETE', `https://http-backend.onrender.com/?${id}`);
 
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
